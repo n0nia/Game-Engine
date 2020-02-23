2f96f9d93beb16312c35f0f02d94faea49e882a1
n0nia <48485245+n0nia@users.noreply.github.com>
2020-02-23 15:05:26 +0200

    Add files via upload

---
+++ Utilities/Camera.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "../Utilities/utilities.h" // if you use STL, please include this line AFTER all other include
+#include "Vertex.h"
+#include "Shaders.h"
+#include <conio.h>
+#include "Globals.h"
+
+
+
